9a68b5016deb3b0bde0dca5e169594ad013eeae5
Louw-C <74646083+Louw-C@users.noreply.github.com>
2024-08-01 11:24:35 +0900

    Changed the order of loading packages.

--- Code/Packages.R
+++ Code/Packages.R
@@ -1,16 +1,16 @@
-require(Rmisc)
-library(readxl)
 library(plyr)
 library(dplyr)
+require(Rmisc)
+library(tidyverse) # this includes ggplot necessary for plotting
+library(readxl)
 library(reshape)
 library(reshape2)
 library(ggplot2)
 library(car)
-library(tidyverse) # this includes ggplot necessary for plotting
 library(EnvStats) # for adding sample size
 library(vctrs)
 require(wesanderson)
 require(hrbrthemes)
 require(vegan)
-library(musculusColors)
+
 
