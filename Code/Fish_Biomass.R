785cebb6b0557d5b8f8be40893d4f39193ee4b22
Louw-C <74646083+Louw-C@users.noreply.github.com>
2024-08-01 12:38:20 +0900

    Uploaded the master file and then looked at the data - aggregated to string. Saw major outliers and removed those - Mola and tiger shark.
    Then - merged the subset data with the meta data and aggregated again.
    
    Plotted biomass across sites and zones.

--- Code/Fish_Biomass.R
+++ Code/Fish_Biomass.R
@@ -2,32 +2,24 @@
 
 #Upload data similar MaxN code (BRUV_2022)
 
-#Load BRUV MaxN and Biomass data
+#Load BRUV MaxN and Biomass data - BRUV_2022 is your master data file.
 BRUV_2022 <- read.csv(file.choose()) 
 BRUV_2022 %>% head(5)
 
-#Load BRUV meta data with depth data
-BRUV_meta <- read.csv(file.choose()) 
-#Look at the first 5 rows of data
-BRUV_meta %>% head(5)
-
-#This merges two data frames with a common ID - here we used Sample
-BRUV_database <- merge(BRUV_2022,BRUV_meta,by = "Sample")
-BRUV_database %>% head(5)
-
 #Group the mean of MaxN and Biomass to String level (mean of 5 rigs to get the string value)
-BRUV_String<-BRUV_database %>% 
-  dplyr::group_by(String, Zone,Binomial, Site, Depth) %>%
-  summarize(string.Biomass_g=mean(Biomass),
-            string.Biomass_kg=mean(Biomass),
+#This averages across strings, and shows 3 data points (strings) per site.
+Biomass_String<-BRUV_2022 %>% 
+  dplyr::group_by(String,Zone,Site) %>%
+  summarize(string.Biomass_g=mean(Biomass_g),
+            string.Biomass_kg=mean(Biomass_kg),
             string.MaxN=mean(MaxN))
 
 #Remove exponential annotation!!!!
 options(scipen = 100, digits = 4)
 
 #Set the order of variables in graphs
-BRUV_String$Zone <- factor(BRUV_String$Zone, levels = c("PNMS North", "DFZ West", "PNMS South"))
-BRUV_String$Site <- factor(BRUV_String$Site, levels = c("N1",
+Biomass_String$Zone <- factor(Biomass_String$Zone, levels = c("PNMS North", "DFZ West", "PNMS South"))
+Biomass_String$Site <- factor(Biomass_String$Site, levels = c("N1",
                                                         "N3",
                                                         "N4",
                                                         "W1",
@@ -42,8 +34,9 @@ BRUV_String$Site <- factor(BRUV_String$Site, levels = c("N1",
 
 #Plot Biomass across zones and sites - just basic plots to check data
 #Data are aggregated (averaged) to string level - so, 3 strings per site (e.g. 3 data points per site)
-BRUV_String %>% 
-  ggplot(aes(x = Site, y=string.Biomass, color=Zone)) +
+#This gives average biomass per string (not total!!!!!!!)
+Biomass_String %>% 
+  ggplot(aes(x = Site, y=string.Biomass_kg, color=Zone)) +
   scale_color_manual(values= wes_palette("FantasticFox1", n = 3))+
   geom_jitter()+
   labs(y = "Biomass (g)")+
@@ -52,30 +45,62 @@ BRUV_String %>%
         axis.text.y= element_text(size=11))
 
 ###Biomass has major outliers - check data
+
 #Tiger shark (Masturus lanceolatus) = 334934.3100
 #Sharptail mola (Masturus lanceolatus) = 134051.6000
 #Blue Marlin (makaira nigricans) = 36646.2900 (Two of them)
 #Silky shark (Carcharhinus falciformis) = 16176.3200
 
+#String biomass of > 19 kg shows these outliers. 
 
 #Subset data to remove tiger shark and Mola.
-####Decide if you want to keep in Marlins and silky shark?####
-Biomass_data<-BRUV_database%>% 
+
+#Remove from BRUV_2022 - and then aggregate again and link with meta data
+
+Biomass_sub<-BRUV_2022%>% 
   subset(Common.name!="tiger shark")
-Biomass_data<-Biomass_data%>% 
+Biomass_sub<-Biomass_data%>% 
   subset(Common.name!="sharptail mola")
 
+#Merge with meta data again - using the subset data.
+BRUV_biomass <- merge(Biomass_sub,BRUV_meta,by = "Sample")
+BRUV_biomass %>% head(5)
 
-#Aggregate to string level for biomass data
-#This averages the five BRUVs for a single string - so you get the biomass for each string
-Biomass_String<-Biomass_data %>% 
-  dplyr::group_by(String, Zone, Site, Depth) %>%
-  summarize(B.Biomass=mean(Biomass),
-            B.MaxN=mean(MaxN))
+#Aggregate again - SUBSET!
+BRUV_String_Sub1<-BRUV_biomass %>% 
+  dplyr::group_by(String.x, Zone,Site, Depth) %>%
+  summarize(string.Biomass_g=mean(Biomass_g),
+            string.Biomass_kg=mean(Biomass_kg),
+            string.MaxN=mean(MaxN))
+
+#Plot the data again with the subset data - removal of two outliers.
+BRUV_String_Sub1 %>% 
+  ggplot(aes(x = Site, y=string.Biomass_g, color=Zone)) +
+  scale_color_manual(values= wes_palette("FantasticFox1", n = 3))+
+  geom_jitter()+
+  labs(y = "Average biomass (g)")+
+  theme(legend.title = element_text(),panel.background = element_blank(),panel.grid.major=element_line(0.5, colour="Gray80"),
+        axis.text.x = element_text(size=11),axis.title.x=element_blank(),
+        axis.text.y= element_text(size=11))
+
+#Look at biomass across depths
+Depth_biomass<-BRUV_String_Sub1 %>% 
+  ggplot(aes(x = Depth, y=string.Biomass_g,color=Zone)) +
+  geom_jitter()+
+  scale_color_manual(values= wes_palette("FantasticFox1", n = 3))+
+  labs(y="Average fish biomass (g)",x="Depth") +
+  theme(axis.text.x=element_text(),strip.text.y = element_text(angle = 0),
+        axis.line = element_line(color='grey'),
+        legend.title = element_text(size=12),
+        panel.background = element_blank(),
+        panel.grid.major=element_line(0.5, colour="Gray80"),
+        panel.grid.minor = element_blank(),
+        legend.text = element_text(size=11))
+Depth_biomass
 
 #Set the order of variables in graphs
-Biomass_String$Zone <- factor(Biomass_String$Zone, levels = c("PNMS North", "DFZ West", "PNMS South"))
-Biomass_String$Site <- factor(Biomass_String$Site, levels = c("N1",
+BRUV_String_Sub1 $Zone <- factor(BRUV_String_Sub1 $Zone, levels = c("PNMS North", "DFZ West", "PNMS South"))
+BRUV_String_Sub1 $Site <- factor(BRUV_String_Sub1 $Site, levels = c("N1",
                                                               "N3",
                                                               "N4",
                                                               "W1",
@@ -87,10 +112,10 @@ Biomass_String$Site <- factor(Biomass_String$Site, levels = c("N1",
                                                               "S3",
                                                               "S4"))
 
-#Plot biomass across sites and zones (same as for the abundance plot above)
-Biomass1.Plot1<-ggplot(BRUV_String, aes(x=factor(Site), y=B.Biomass, fill=Zone))+
+#Boxplot of biomass across sites and zones (same as for the abundance plot above)
+Biomass1.Plot1<-ggplot(BRUV_String_Sub1, aes(x=factor(Site), y=string.Biomass_g, fill=Zone))+
   geom_boxplot()+geom_jitter(colour="darkgrey",shape=5)+
-  labs(y = "Biomass (g)")+
+  labs(y = "Average biomass (g)")+
   scale_fill_manual(values= wes_palette("FantasticFox1", n = 3))+
   theme(axis.line = element_line(color='grey'),
         legend.title = element_text(),panel.background = element_blank(),panel.grid.major=element_line(0.5, colour="Gray80"),
@@ -99,9 +124,9 @@ Biomass1.Plot1<-ggplot(BRUV_String, aes(x=factor(Site), y=B.Biomass, fill=Zone))
 Biomass1.Plot1
 
 #Look at biomass across zones only - grouped
-Biomass2.Plot1<-ggplot(Biomass_String, aes(x=factor(Zone), y=B.Biomass, fill=Zone))+
+Biomass2.Plot1<-ggplot(BRUV_String_Sub1, aes(x=factor(Zone), y=string.Biomass_g, fill=Zone))+
   geom_boxplot()+geom_jitter(colour="darkgrey", shape=5)+
-  labs(y = "Biomass (g)")+
+  labs(y = "Average biomass (g)")+
   scale_fill_manual(values= wes_palette("Zissou1", n = 3))+
   theme(axis.line = element_line(color='grey'),
         legend.title = element_blank(),legend.position="none",panel.background = element_blank(),panel.grid.major=element_line(0.5, colour="Gray80"),
@@ -110,8 +135,15 @@ Biomass2.Plot1<-ggplot(Biomass_String, aes(x=factor(Zone), y=B.Biomass, fill=Zon
 Biomass2.Plot1
 
 #Look at Biomass for each taxa across sites
+#Aggregate again with Binomial
+BRUV_String_Sub2<-BRUV_biomass %>% 
+  dplyr::group_by(String.x, Zone,Site,Binomial,Depth) %>%
+  summarize(string.Biomass_g=mean(Biomass_g),
+            string.Biomass_kg=mean(Biomass_kg),
+            string.MaxN=mean(MaxN))
+
 #The taxa are averaged across a string.
-TaxaBiomass.Plot1<-ggplot(BRUV_taxa, aes(x=factor(Binomial), y=Taxa.Biomass))+
+TaxaBiomass.Plot1<-ggplot(BRUV_String_Sub2, aes(x=factor(Binomial), y=string.Biomass_g))+
   facet_grid(Zone~.)+
   geom_boxplot()+geom_jitter(colour="darkgrey", shape=5)+
   labs(y = "Relative fish abundance (MaxN)")+
@@ -120,3 +152,4 @@ TaxaBiomass.Plot1<-ggplot(BRUV_taxa, aes(x=factor(Binomial), y=Taxa.Biomass))+
         axis.text.x = element_text(angle=90,vjust=0.3,face="italic"),axis.title.x=element_blank(),
         axis.text.y= element_text(size=11),panel.grid.minor = element_blank())
 TaxaBiomass.Plot1
+#Not really informative - so not saving a graph. 
